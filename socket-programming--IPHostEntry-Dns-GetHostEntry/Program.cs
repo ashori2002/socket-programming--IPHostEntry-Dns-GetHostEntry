74bc8aafb46f6096b84876365d20ebf6bf000f59
ashori2002 <mohammadashori2002@gmail.com>
2023-05-05 00:32:48 +0430

    Add project files.

---
+++ socket-programming--IPHostEntry-Dns-GetHostEntry/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace socket_programming__IPHostEntry_Dns_GetHostEntry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
