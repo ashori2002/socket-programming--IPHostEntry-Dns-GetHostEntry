46e84bcddbebcb4ef52e4a00327809322e6368bf
ashori2002 <mohammadashori2002@gmail.com>
2023-05-05 00:35:54 +0430

    IPHostEntry

--- socket-programming--IPHostEntry-Dns-GetHostEntry/Program.cs
+++ socket-programming--IPHostEntry-Dns-GetHostEntry/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace socket_programming__IPHostEntry_Dns_GetHostEntry
 {
@@ -10,6 +11,8 @@ namespace socket_programming__IPHostEntry_Dns_GetHostEntry
     {
         static void Main(string[] args)
         {
+            IPHostEntry HE = Dns.GetHostEntry("127.0.0.1");
+
         }
     }
 }
